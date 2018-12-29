921077a63c4f7a5e544c462de814dd4a83a71862
Bhagyashree18 <bhagyashreemhetre@gmail.com>
2018-12-29 13:23:50 +0530

    First Commit

---
+++ syso/src/test/java/print/Hello.java
@@ -0,0 +1,12 @@
+package print;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The Project is Developed");
+		System.out.println("Start Testing");
+
+	}
+
+}
